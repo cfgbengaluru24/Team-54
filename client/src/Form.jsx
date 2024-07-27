321577967f1e2a9e8be5263d2ea16792fd29ce87
Gireesh-KL <gireeshkl257@gmail.com>
2024-07-27 22:52:16 +0530

    validated endpoints

--- client/src/Form.jsx
+++ client/src/Form.jsx
@@ -1,130 +1,102 @@
-import react from "react";
-import "./App.css";
+import React from "react";
 import axios from "axios";
 import Container from "@mui/material/Container";
 import FormControl from "@mui/material/FormControl";
-import { TextField } from "@mui/material";
+import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { useForm } from "react-hook-form";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import "./App.css";
 
 function App() {
-	/*const [data, setData] = react.useState(null);
+    const { register, handleSubmit } = useForm();
 
-	react.useEffect(() => {
-		axios.get("/api").then((res) => setData(res.data.message));
-	}, []);
-*/
+    const onSubmit = (data) => {
+        axios.post("http://localhost:3000/edu/data", data)
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
+    };
 
-	const { register, handleSubmit } = useForm();
-
-	const onsub = (data) => {
-		axios.post("http://localhost:3001/api", data)
-			.then(response => {
-				console.log(response.data);
-			})
-			.catch(error => {
-				console.error('There was an error!', error);
-			});
-	};
-
-	const handleSpeak = (text) => {
+    const handleSpeak = (text) => {
         const utterance = new SpeechSynthesisUtterance(text);
         window.speechSynthesis.speak(utterance);
     };
-	return (
-		// <Router>
-		// 	<Routes>
-		// 		<Route exact path="/" element={<Main/>}/>
-		// 		<Route exact path="/about" element={<Login/>}/>
-		// 	</Routes>
-		// </Router>
-		<div
-		style={{
-			display: "flex",
-			alignItems: "center",
-			justifyContent: "center",
-			background: "#CFFCF8",
-			height: "100vh",
-			margin: 0,
-		  }}
-		>
-			<Container
-				maxWidth="xs"
-				style={{
-					background: "white",
-					height: "24rem",
-					borderRadius: "20px",
-				}}
-			>
-				<form
-					onSubmit={handleSubmit((data) => {
-						onsub(data);
-					})}
-				>
-					<FormControl>
-						<div
-							style={{
-								display: "flex",
-								flexDirection: "row",
-								justifyContent: "space-evenly",
-							}}
-						>
-							<TextField
-								{...register("child-id")}
-								style={{ marginTop: "20px", marginRight: "10px" }}
-								id="child-id"
-								name="child-id"
-								label="Child Id"
-								variant="outlined"
-							/>
-							<TextField
-								{...register("school")}
-								style={{ marginTop: "20px" }}
-								id="school"
-								name="school"
-								label="School Name"
-								variant="outlined"
-							/>
-						</div>
-						<TextField
-							{...register("marks")}
-							style={{ marginTop: "20px" }}
-							id="marks"
-							name="marks"
-							label="Marks"
-							variant="outlined"
-						/>
-						<TextField
-							{...register("bg")}
-							style={{ marginTop: "20px" }}
-							name="bg"
-							label="Blood Group"
-							variant="outlined"
-						/>
-						<div
-							style={{
-								marginTop: "50px",
-								display: "flex",
-								justifyContent: "center",
-								alignItems: "center",
-								height: "100%",
-							}}
-						>
-							<Button
-								type="submit"
-								variant="contained"
-								style={{ marginTop: "10px", width: "40%" }}
-							>
-								Submit
-							</Button>
-						</div>
-					</FormControl>
-				</form>
-			</Container>
-		</div>
-	);
+
+    return (
+        <div
+            style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                background: "#CFFCF8",
+                height: "100vh",
+                margin: 0,
+            }}
+        >
+            <Container
+                maxWidth="xs"
+                style={{
+                    background: "white",
+                    height: "24rem",
+                    borderRadius: "20px",
+                    padding: "20px"
+                }}
+            >
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <FormControl fullWidth>
+                        <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+                            <TextField
+                                {...register("studentId")}
+                                style={{ marginTop: "20px", marginRight: "10px" }}
+                                id="studentId"
+                                name="studentId"
+                                label="Student Id"
+                                variant="outlined"
+                            />
+                            <TextField
+                                {...register("name")}
+                                style={{ marginTop: "20px" }}
+                                id="name"
+                                name="name"
+                                label="Name"
+                                variant="outlined"
+                            />
+                        </div>
+                        <TextField
+                            {...register("report")}
+                            style={{ marginTop: "20px" }}
+                            id="report"
+                            name="report"
+                            label="Report"
+                            variant="outlined"
+                            multiline
+                            rows={4}
+                        />
+                        <div
+                            style={{
+                                marginTop: "20px",
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                height: "100%",
+                            }}
+                        >
+                            <Button
+                                type="submit"
+                                variant="contained"
+                                style={{ marginTop: "10px", width: "40%" }}
+                            >
+                                Submit
+                            </Button>
+                        </div>
+                    </FormControl>
+                </form>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
